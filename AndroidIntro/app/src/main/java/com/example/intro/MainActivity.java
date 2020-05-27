61fbd948fb009348534a2bc877a938d52ab33bd8
Sergio <miemail@gmail.com>
2020-05-26 20:55:09 -0500

    First Demo to ML using Java

--- AndroidIntro/app/src/main/java/com/example/intro/MainActivity.java
+++ AndroidIntro/app/src/main/java/com/example/intro/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.intro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit_text);
+        textView = findViewById(R.id.text_view);
+    }
+
+    public void displayResults(View view) {
+        String inputString =  editText.getText().toString();
+        if (inputString.isEmpty()) {
+            return;
+        }
+        textView.setText(inputString);
     }
 }
